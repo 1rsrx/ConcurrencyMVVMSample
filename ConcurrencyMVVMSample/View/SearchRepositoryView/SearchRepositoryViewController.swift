6454ae154bd462d01285ea5ae135a73da9ae3436
HikaruKurodq <h1kqaa@gmail.com>
2023-02-02 08:38:08 +0900

    ViewModelを作成

--- ConcurrencyMVVMSample/View/SearchRepositoryView/SearchRepositoryViewController.swift
+++ ConcurrencyMVVMSample/View/SearchRepositoryView/SearchRepositoryViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class SearchRepositoryViewController: UIViewController {
 
@@ -13,6 +14,9 @@ class SearchRepositoryViewController: UIViewController {
     @IBOutlet weak var repositoryTableView: UITableView!
     private var loadingLabel: UILabel!
 
+    private var viewModel: SearchRepositoryViewModel!
+    private var cancellables: [AnyCancellable] = []
+    private let repositoryTableViewCellIdentifier = "RepositoryTableViewCell"
 
     override func loadView() {
         let view = UINib(nibName: "SearchRepositoryView", bundle: nil)
@@ -23,6 +27,96 @@ class SearchRepositoryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        viewModel = SearchRepositoryViewModel()
+        setupView()
+        bind()
+    }
+
+    private func setupView() {
+        searchBar.delegate = self
+
+        repositoryTableView.register(
+            UINib(nibName: "RepositoryTableViewCell", bundle: nil),
+            forCellReuseIdentifier: repositoryTableViewCellIdentifier
+        )
+        repositoryTableView.dataSource = self
+
+        loadingLabel = UILabel()
+        loadingLabel.text = "通信中"
+        loadingLabel.textColor = .black
+        loadingLabel.isHidden = true
+        view.addSubview(loadingLabel)
+        loadingLabel.translatesAutoresizingMaskIntoConstraints = false
+        loadingLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        loadingLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+
+    private func bind() {
+        viewModel.$repositories
+            .sink { [weak self] in
+                self?.reloadRepositoryTableView($0)
+            }
+            .store(in: &cancellables)
+
+        viewModel.$errorMessage
+            .sink { [weak self] in
+                self?.showAlert(title: "エラー", message: $0)
+            }
+            .store(in: &cancellables)
+
+        viewModel.$isLoading
+            .sink { [weak self] in
+                self?.toggleItemHidden(by: $0)
+            }
+            .store(in: &cancellables)
+    }
+
+    private func reloadRepositoryTableView(_ repositories: [Repository]) {
+        self.repositoryTableView.reloadData()
+    }
+
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let closeAction = UIAlertAction(title: "閉じる", style: .default)
+        alert.addAction(closeAction)
+        self.present(alert, animated: true)
+    }
+
+    private func toggleItemHidden(by isLoading: Bool) {
+        self.repositoryTableView.isHidden = isLoading
+        self.loadingLabel.isHidden = !isLoading
     }
 }
 
+extension SearchRepositoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.repositories.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: repositoryTableViewCellIdentifier,
+            for: indexPath
+        ) as! RepositoryTableViewCell
+
+        let repository = viewModel.repositories[indexPath.row]
+        cell.configureWith(repository: repository)
+
+        return cell
+    }
+}
+
+extension SearchRepositoryViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        viewModel.onSearchTextChanged(searchText)
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.endEditing(true)
+
+        Task {
+            await viewModel.onSearchButtonTapped()
+        }
+
+    }
+}
